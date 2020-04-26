6c40ee015ac2149fe123ba933f3246b187599e5f
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 20:35:24 +0700

    Add MovieItem CSS

--- src/containers/MovieItem/MovieItem.js
+++ src/containers/MovieItem/MovieItem.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import MoviePlayer from "../../components/Movie/MoviePlayer/MoviePlayer";
 import MovieInfo from "../../components/Movie/MovieInfo/MovieInfo";
 
+import styles from "./MovieItem.module.scss";
+
 class MovieItem extends Component {
   render() {
     return (
-      <div>
-        <MoviePlayer />
-        <MovieInfo />
+      <div className={styles.MovieItem}>
+        <MoviePlayer id="LdOM0x0XDMo" />
+        <MovieInfo
+          title="TENET - Official Trailer"
+          author="Shared by: bolobala"
+          description="asdfhasdgfhgasdjkfhasdjk"
+        />
       </div>
     );
   }
