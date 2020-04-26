0f55b8b8f2b0cc1557e3817ad33e8f66973fa1c8
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 23:09:13 +0700

    Import NavBar to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
 import Layout from "./components/Layout/Layout";
+import NavBar from "./components/NavBar/NavBar";
 import MovieList from "./containers/MovieList/MovieList";
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <div>Toolbar</div>
+        <NavBar />
+        <hr/>
         <MovieList />
       </Layout>
     );
