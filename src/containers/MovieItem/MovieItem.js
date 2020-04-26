0d3a52db30730962f435f2ba70328be40b966ec0
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 17:02:01 +0700

    Add MovieItem container

---
+++ src/containers/MovieItem/MovieItem.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class MovieItem extends Component {
+  render () {
+    return (
+      <Fragment>
+        <div>Movie Player</div>
+        <div>Movie Info</div>
+      </Fragment>
+    );
+  }
+}
+
+export default MovieItem;
