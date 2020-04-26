821abb7f28e1a96edb2494b9c19d5a2330d82a6a
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 20:35:02 +0700

    Update MovieInfo component and CSS

--- src/components/Movie/MovieInfo/MovieInfo.js
+++ src/components/Movie/MovieInfo/MovieInfo.js
@@ -10,7 +10,7 @@ const MovieInfo = ({ title, author, description }) => {
     <div className={styles.MovieInfo}>
       <div className={styles.MovieTitle}>{title}</div>
       <div className={styles.MovieAuthor}>{author}</div>
-      <div className={styles.MovieDescriptionText}>'Description:</div>
+      <div className={styles.MovieDescriptionText}>Description:</div>
       <div className={styles.MovieDescription}>{description}</div>
     </div>
   );
