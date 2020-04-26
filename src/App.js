126c52e572e25c0bdd4fde1cfe17b76bbc022130
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 00:46:20 +0700

    Import react-router to render component based on url

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import NavBar from "./components/NavBar/NavBar";
 import MovieList from "./containers/MovieList/MovieList";
+import ShareMovie from "./components/ShareMovie/ShareMovie";
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <NavBar />
-        <hr/>
-        <MovieList />
+        <Router>
+          <div>
+            <NavBar />
+            <hr />
+            <Switch>
+              <Route exact path="/" component={withRouter(MovieList)}></Route>
+              <Route
+                exact
+                path="/share"
+                component={withRouter(ShareMovie)}
+              ></Route>
+            </Switch>
+          </div>
+        </Router>
       </Layout>
     );
   }
