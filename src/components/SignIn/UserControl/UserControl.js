ccdd631159537048664d96285f970f443929eea8
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 00:45:28 +0700

    Update UserControl component

--- src/components/SignIn/UserControl/UserControl.js
+++ src/components/SignIn/UserControl/UserControl.js
@@ -1,14 +1,17 @@
 import React, { Component, Fragment } from "react";
+
 import styles from "./UserControl.module.scss";
 
 class UserControl extends Component {
   render() {
-    const { username } = this.props
+    const { username } = this.props;
 
     return (
       <Fragment>
         <div className={styles.Welcome}>{`Welcome ${username}`}</div>
-        <button>Share a movie</button>
+        <button>
+          <a href="/share">Share a movie</a>
+        </button>
         <button>Logout</button>
       </Fragment>
     );
