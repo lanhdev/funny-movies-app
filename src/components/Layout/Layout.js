ca558c19c781d94e400544a9c0609156a2ee44ca
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 16:09:31 +0700

    Create the Layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const layout = (props) => (
+  <Fragment>
+    <div>Toolbar</div>
+    <main>{props.children}</main>
+  </Fragment>
+);
+
+export default layout;
