3e5ad05db751065ffc1744a9bf157a73c92ab830
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 17:32:41 +0700

    Modify MovieInfo component

--- src/components/Movie/MovieInfo/MovieInfo.js
+++ src/components/Movie/MovieInfo/MovieInfo.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./MovieInfo.module.css";
 
-const MovieInfo = (props) => {
+const MovieInfo = ({ title, author, showDescription = false, description }) => {
   return (
     <div className={styles.MovieInfo}>
-      <div className={styles.MovieTitle}>{props.title}</div>
-      <div className={styles.MovieAuthor}>{props.author}</div>
-      <div className={styles.MovieDescriptionText}>Description:</div>
-      <div className={styles.MovieDescription}>{props.description}</div>
+      <div className={styles.MovieTitle}>{title}</div>
+      <div className={styles.MovieAuthor}>{author}</div>
+      <div className={styles.MovieDescriptionText}>{showDescription ? 'Description:' : ''}</div>
+      <div className={styles.MovieDescription}>{description}</div>
     </div>
   );
 };
