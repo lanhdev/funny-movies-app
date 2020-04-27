8b898bee72f8833cc0e8fdc2fd6a67e0dd7e99fc
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 14:17:47 +0700

    Add SIGN_IN action to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,25 @@
 const initialState = {
-
+  currentUser: {
+    id: null,
+    username: '',
+    authToken: '',
+    isSignedIn: false
+  }
 }
 
 const reducer = (state = initialState, action) => {
+  if (action.type === 'SIGN_IN') {
+    return {
+      ...state,
+      currentUser: {
+        ...state.currentUser,
+        id: action.payload.id,
+        username: action.payload.username,
+        authToken: action.payload.authToken,
+        isSignedIn: action.payload.isSignedIn
+      }
+    }
+  }
   return state;
 }
 
