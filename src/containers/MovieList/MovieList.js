dc19e0e44c3cadf3513bae84d6bb78327c34e163
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 09:48:41 +0700

    Fetch movie data from API and display in MovieList container

--- src/containers/MovieList/MovieList.js
+++ src/containers/MovieList/MovieList.js
@@ -1,18 +1,32 @@
 import React, { Component, Fragment } from "react";
+import axios from "axios";
 
 import MovieItem from "../../components/MovieItem/MovieItem";
 
-const movieItems = [{ id: "LdOM0x0XDMo" }, { id: "sfM7_JLk-84" }];
-
 class MovieList extends Component {
+  state = {
+    movieItems: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://funny-movies-api.test/movies").then((response) => {
+      this.setState({ movieItems: response.data });
+    });
+  }
+
   render() {
-    return (
-      <Fragment>
-        {movieItems.map((item) => (
-          <MovieItem key={item.id} movie_id={item.id} />
-        ))}
-      </Fragment>
-    );
+    const movieItems = this.state.movieItems.map((movieItem) => {
+      const url = movieItem.url.split("=")[1];
+      return (
+        <MovieItem
+          key={movieItem.id}
+          movie_id={url}
+          author={movieItem.username}
+        />
+      );
+    });
+
+    return <Fragment>{movieItems}</Fragment>;
   }
 }
 
