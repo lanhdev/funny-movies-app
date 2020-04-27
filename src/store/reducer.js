f9999a83ebf9b5f88bbb51e9cd7de95ae7666530
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 15:05:23 +0700

    Add action SIGN_OUT to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,26 +1,39 @@
 const initialState = {
   currentUser: {
     id: null,
-    userName: '',
-    authToken: '',
-    isSignedIn: false
-  }
-}
+    userName: "",
+    authToken: "",
+    isSignedIn: false,
+  },
+};
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'SIGN_IN') {
-    return {
-      ...state,
-      currentUser: {
-        ...state.currentUser,
-        id: action.payload.id,
-        userName: action.payload.userName,
-        authToken: action.payload.authToken,
-        isSignedIn: action.payload.isSignedIn
-      }
-    }
+  switch (action.type) {
+    case "SIGN_IN":
+      return {
+        ...state,
+        currentUser: {
+          ...state.currentUser,
+          id: action.payload.id,
+          userName: action.payload.userName,
+          authToken: action.payload.authToken,
+          isSignedIn: action.payload.isSignedIn,
+        },
+      };
+    case "SIGN_OUT":
+      return {
+        ...state,
+        currentUser: {
+          ...state.currentUser,
+          id: null,
+          userName: "",
+          authToken: "",
+          isSignedIn: false,
+        },
+      };
+    default:
+      return state;
   }
-  return state;
-}
+};
 
 export default reducer;
