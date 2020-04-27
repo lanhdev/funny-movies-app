339a67d2609ed80c214316d10a039dcc4fa6c7e6
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 14:48:54 +0700

    Integrate post movie API to ShareMovie component

--- src/components/ShareMovie/ShareMovie.js
+++ src/components/ShareMovie/ShareMovie.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import styles from "./ShareMovie.module.scss";
+import axios from "axios";
 
 class ShareMovie extends Component {
+  state = {
+    url: "",
+  };
+
+  shareMovieHandler = (event) => {
+    event.preventDefault();
+    const shareMovieData = {
+      url: this.state.url,
+    };
+    const axiosConfig = {
+      headers: {
+        Authorization: "Bearer " + this.props.authToken,
+      },
+    };
+
+    axios
+      .post("http://funny-movies-api.test/movies", shareMovieData, axiosConfig)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
   render() {
+    const { isSignedIn } = this.props;
+
+    if (!isSignedIn) {
+      return null;
+    }
+
     return (
       <div className={styles.Container}>
         <div className={styles.Box}>
@@ -17,8 +50,11 @@ class ShareMovie extends Component {
               name="url"
               placeholder="Your YouTube URL"
               required
+              onChange={(event) => this.setState({ url: event.target.value })}
             />
-            <button type="submit">Share</button>
+            <button type="submit" onClick={this.shareMovieHandler}>
+              Share
+            </button>
           </form>
         </div>
       </div>
@@ -26,4 +62,11 @@ class ShareMovie extends Component {
   }
 }
 
-export default ShareMovie;
+const mapStateToProps = (state) => {
+  return {
+    isSignedIn: state.currentUser.isSignedIn,
+    authToken: state.currentUser.authToken,
+  };
+};
+
+export default connect(mapStateToProps)(ShareMovie);
