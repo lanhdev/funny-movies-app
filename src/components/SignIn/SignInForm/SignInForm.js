e5c78649bf99f5933a548f8880b8b6d7835a22d9
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 11:28:58 +0700

    Integrate sign in API to SignInForm component

--- src/components/SignIn/SignInForm/SignInForm.js
+++ src/components/SignIn/SignInForm/SignInForm.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
 import styles from "./SignInForm.module.scss";
 
+import axios from "axios";
+
 class SignInForm extends Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  signInHandler = (event) => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    const userData = {
+      user: {
+        username: username,
+        password: password,
+      },
+    };
+
+    axios
+      .post("http://funny-movies-api.test/sign_in", userData)
+      .then((response) => {
+        console.log(response.headers.authorization.split(' ')[1]);
+      });
+  };
+
   render() {
+    const { username, password } = this.state;
+
     return (
       <div>
         <form>
@@ -12,6 +38,10 @@ class SignInForm extends Component {
             name="username"
             placeholder="username"
             required
+            value={username}
+            onChange={(event) =>
+              this.setState({ username: event.target.value })
+            }
           />
           <input
             className={styles.Password}
@@ -19,8 +49,14 @@ class SignInForm extends Component {
             name="password"
             placeholder="password"
             required
+            value={password}
+            onChange={(event) =>
+              this.setState({ password: event.target.value })
+            }
           />
-          <button type="submit">Sign In / Register</button>
+          <button type="submit" onClick={this.signInHandler}>
+            Log In / Register
+          </button>
         </form>
       </div>
     );
