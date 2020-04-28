aa58bda64b3786ff4e2107f7117ec1ee0c3587de
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-28 21:38:20 +0700

    Add MoviePlayer.test.js

---
+++ src/components/MovieItem/MoviePlayer/MoviePlayer.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import MoviePlayer from "./MoviePlayer";
+
+configure({ adapter: new Adapter() });
+
+describe("<MoviePlayer />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<MoviePlayer />);
+  });
+
+  it("should not render if props is null", () => {
+    expect(wrapper.find("iframe").exists()).toEqual(false);
+  });
+
+  it("should have iframe tag if props is present", () => {
+    wrapper.setProps({ movie_id: 1 });
+    expect(wrapper.find("iframe").exists()).toEqual(true);
+  });
+});
