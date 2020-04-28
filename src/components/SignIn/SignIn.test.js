11719b206cdab8ca3b569923f5378df4878a7225
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-28 20:56:10 +0700

    Format SignIn.test.js

--- src/components/SignIn/SignIn.test.js
+++ src/components/SignIn/SignIn.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+
 import { SignIn } from "./SignIn";
 import SignInForm from "./SignInForm/SignInForm";
 import UserControl from "./UserControl/UserControl";
