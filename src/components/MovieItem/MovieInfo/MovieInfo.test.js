7e50efa40579d0742fc93cf564c147e9b1ee2436
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-28 21:43:43 +0700

    Add MovieInfo.test.js

---
+++ src/components/MovieItem/MovieInfo/MovieInfo.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import MovieInfo from "./MovieInfo";
+
+configure({ adapter: new Adapter() });
+
+describe("<MovieInfo />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<MovieInfo />);
+  });
+
+  it("should not render if title is null", () => {
+    expect(wrapper.find("div").exists()).toEqual(false);
+  });
+
+  it("should have div tag if title is present", () => {
+    wrapper.setProps({
+      title: "title",
+      author: "author",
+      description: "description",
+    });
+    console.log(wrapper.find("div"));
+    expect(wrapper.find("div")).toHaveLength(5);
+  });
+});
