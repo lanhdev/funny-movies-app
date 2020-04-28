02024f86165a0c88922755ed31f9a6b5c3337da5
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-28 17:41:14 +0700

    Modify App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App';
+import Layout from './components/Layout/Layout';
+import NavBar from "./components/NavBar/NavBar";
+
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it ('should render Layout and Navbar components', () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.find(Layout).exists()).toBeTruthy();
+    expect(wrapper.find(NavBar).exists()).toBeTruthy();
+  })
+})
+
