c84fe38a58e897a410d12f338d831912e6219ba9
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-29 23:30:38 +0700

    Add youtube constants to config

--- src/config.js
+++ src/config.js
@@ -10,5 +10,7 @@ const config = process.env.NODE_ENV === "production" ? prod : dev;
 
 export default {
   ...config,
-  BASE_EMBED_URL: "https://www.youtube.com/embed/",
+  YOUTUBE_EMBED_URL: "https://www.youtube.com/embed/",
+  YOUTUBE_API_URL: "https://www.googleapis.com/youtube/v3/videos",
+  YOUTUBE_API_KEY: "AIzaSyBS5QAF7S5mw3Ba4x--AfW3T40CjWRK5jQ",
 };
